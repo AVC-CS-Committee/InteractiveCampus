0dca0947a8f256ead0a04ac714939e476b13086d
KallosP <97070073+KallosP@users.noreply.github.com>
2023-04-03 22:47:02 -0700

    Testing
    -this is a test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_config/flutter_config.dart';
-
+// TEST PUSH
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
