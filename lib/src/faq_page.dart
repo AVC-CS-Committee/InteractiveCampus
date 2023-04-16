d44a22a7d2a7f7d7cafddcc8146cf454b821fae1
ryanreevess <rreeves5@avc.edu>
2023-04-15 17:25:51 -0700

    Added FAQ page functionality

--- lib/src/faq_page.dart
+++ lib/src/faq_page.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class FAQPage extends StatelessWidget {
-  const FAQPage({Key? key}) : super(key: key);
+class FAQPage extends StatefulWidget {
+  @override
+  _FAQPageState createState() => _FAQPageState();
+}
+
+class _FAQPageState extends State<FAQPage> {
+  List<dynamic> faqs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFAQs();
+  }
+
+  Future<void> _loadFAQs() async {
+    String jsonString =
+        await rootBundle.loadString('assets/json/faq_questions.json');
+    setState(() {
+      faqs = json.decode(jsonString);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
-        title: const Text('Frequently Asked Questions',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Sans Serif',
-            )),
-        centerTitle: true,
-        elevation: 2,
+        title: const Text('FAQs'),
       ),
-      body: Center(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
-            ExpansionTile(
-              title: Text('Question 1'),
-              children: [
-                ListTile(
-                  title: Text('Answer 1'),
-                ),
-              ],
+      body: faqs.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: faqs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ExpansionTile(
+                  title: Text(
+                    faqs[index]['question'],
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 24.0),
+                      child: Text(
+                        faqs[index]['answer'],
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-            Divider(),
-            ExpansionTile(
-              title: Text('Question 2'),
-              children: [
-                ListTile(
-                  title: Text('Answer 2'),
-                ),
-              ],
-            ),
-            Divider(),
-            ExpansionTile(
-              title: Text('Question 3'),
-              children: [
-                ListTile(
-                  title: Text('Answer 3'),
-                ),
-              ],
-            ),
-            Divider(),
-          ],
-        ),
-      ),
     );
   }
 }
