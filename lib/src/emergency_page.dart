004ab9351c9815d8c5534f17b85982da80cc8c8f
ryanreevess <rreeves5@avc.edu>
2023-04-18 23:23:47 -0700

    More work on the Emergency Page

--- lib/src/emergency_page.dart
+++ lib/src/emergency_page.dart
@@ -9,7 +9,7 @@ class EmergencyPage extends StatelessWidget {
         iconTheme: const IconThemeData(
           color: Colors.white,
         ),
-        title: const Text('Emergency',
+        title: const Text('Emergency Contacts',
             style: TextStyle(
               color: Colors.white,
               fontFamily: 'Sans Serif',
@@ -21,19 +21,19 @@ class EmergencyPage extends StatelessWidget {
         children: [
           _buildSection(
             context,
-            'Call Emergency Services',
-            'Call 911 or your local emergency number immediately if you are in a life-threatening or emergency situation.',
+            'AVC Sheriff\'s Office',
+            'Monday through Friday: 7:30 am to Midnight After midnight, the office is closed but officers are on patrol throughout the campus. You can contact them after midnight and up until 7:30 am the following morning from any payphone at *80 or from your cell phone at 661.722.6399.\n\nSaturday and Sunday The office is closed on Saturday and Sunday but officers are on patrol throughout the campus. You can contact them from any payphone at *80, or from your cell phone at 661.722.6399.\n\nLocation The Antelope Valley College safety and security department is located in the Student Services Center on the south side of the building in room T800.',
             '911', // add emergency number here
           ),
           _buildSection(
             context,
-            'Emergency Contacts',
+            'Campus Safety Escort Program',
             'Add emergency contacts to your phone and keep them updated in case of an emergency.',
             '123-456-7890', // add emergency number here
           ),
           _buildSection(
             context,
-            'Emergency Kit',
+            'Criminal Prevention and',
             'Prepare an emergency kit with essential items like food, water, first aid supplies, and important documents. Keep it in a safe and easily accessible place.',
             '999-999-9999', // add emergency number here
           ),
@@ -54,13 +54,14 @@ class EmergencyPage extends StatelessWidget {
             style: const TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
+              color: Color(0xff8d1c40),
             ),
           ),
           const SizedBox(height: 16),
           Text(
             description,
             style: const TextStyle(
-              fontSize: 18,
+              fontSize: 14,
             ),
           ),
           const SizedBox(height: 16),
