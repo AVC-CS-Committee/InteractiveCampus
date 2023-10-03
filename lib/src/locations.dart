207f75a0d1e0efc2fc2528060b3afafbbac0b9cc
busterblake <33817212+busterblake@users.noreply.github.com>
2023-10-02 18:09:15 -0700

    blake stuff

--- lib/src/locations.dart
+++ lib/src/locations.dart
@@ -1,10 +1,16 @@
 import 'dart:convert';
 import 'dart:developer';
-
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:image/image.dart' as img;
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'location_descriptions.dart';
 
@@ -60,13 +66,40 @@ Future<Set<Marker>> getMarkers(BuildContext context) async {
           'https://raw.githubusercontent.com/AVC-CS-Committee/InteractiveCampusMap/master/app/src/main/res/drawable/image_$image.jpg');
     }
 
+
+Future<BitmapDescriptor> createCustomMarkerIcon(BuildContext context, String assetPath, double iconSize) async {
+  final ByteData? byteData = await rootBundle.load(assetPath);
+
+  if (byteData == null) {
+    throw FlutterError('Failed to load asset: $assetPath');
+  }
+
+  final Uint8List uint8List = byteData.buffer.asUint8List();
+
+  // Load the image using the image package
+  final img.Image? image = img.decodeImage(uint8List);
+
+  if (image == null) {
+    throw FlutterError('Failed to decode image');
+  }
+
+  // Convert the image to RGBA
+  final img.Image rgbaImage = img.copyRotate(image, 0); // Ensure it's not rotated
+  final Uint8List rgbaUint8List = Uint8List.fromList(img.encodePng(rgbaImage));
+
+  return BitmapDescriptor.fromBytes(rgbaUint8List);
+}
+
+
+
     BitmapDescriptor markerIcon;
 
      // Set custom marker icon based on location type
     if (location.type == "parking") {
-      markerIcon = await BitmapDescriptor.fromAssetImage(
-          const ImageConfiguration(),
-            'assets/images/parking.png');
+
+      double iconSize = MediaQuery.of(context).size.width * 0.05; // Calculate the desired icon size
+      markerIcon = await createCustomMarkerIcon(context, 'assets/images/parking.png', iconSize);
+
     } else if (location.type == "classroom") {
       markerIcon = await BitmapDescriptor.fromAssetImage(
           const ImageConfiguration(),
